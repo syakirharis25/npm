41ba7a64fe6ebe310e3fea1233f67de180ae8c90
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-14 06:03:10 +0800

    adding moment.js and browserify practices files

---
+++ main.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+var unique = require('uniq');
+
+var myDate = new Date();
+var myCoolDate = moment(myDate).format('LL');
+var myList = [1, 2, 1, 1, 1, 3, 3, 3, 4, 5, 6, 4, 4, 7];
+
+var myUniqueList = unique(myList);
+
+console.log(myUniqueList);
